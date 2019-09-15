9cc01d6ccf58721d749bf4d634a0fb81dced556d
bright okike <brightokike@gmail.com>
2019-09-15 09:52:52 +0300

    working on the UI

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+ 
   def index
     @post = Post.new
     @posts = Post.all.order("created_at DESC")
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  root to: 'home#index'
+  get 'home' => 'home#index'
+  root to: 'posts#index'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'signup', sign_up: 'cmon_let_me_in' }
 
   resource :posts
