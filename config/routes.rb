ccdaba2489b65205b717d79bb6d1b330cd341e7f
bright okike <brightokike@gmail.com>
2019-09-15 11:02:30 +0300

    sign UI implement

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home' => 'home#index'
-  root to: 'posts#index'
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'signup', sign_up: 'cmon_let_me_in' }
+  root to: 'home#index'
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'signup', sign_up: 'new' }
 
   resource :posts
 end
