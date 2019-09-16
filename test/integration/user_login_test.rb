11cea2f15a5d9ed8b467a2c911a34c18db43f858
Kelvin Liang <kelvin9877@gmail.com>
2019-09-16 19:07:08 +0800

    fix test error

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -8,7 +8,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "Have to login in root page" do
     get root_url
-    assert_select 'a[href=?]', new_user_session_path, count: 2
-    assert_select 'a[href=?]', new_user_registration_path, count: 1
+    assert_select 'a[href=?]', new_user_session_path, count: 1
+    
   end
 end
