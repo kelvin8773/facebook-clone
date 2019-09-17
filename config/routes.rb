41fc12fe81abca5a0c0914ab47a2e98cdffd08d6
bright okike <brightokike@gmail.com>
2019-09-17 19:57:28 +0300

    the user controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/index'
   get 'home' => 'home#index'
   root to: 'home#index'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'signup', sign_up: 'new' }
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
   delete "/delete_post", to: "posts#destroy"
 
   resource :posts
+  resources :users, only: [:index]
 end
