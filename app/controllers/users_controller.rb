cc9581201caf4525b73465fdc04ca24edac1128e
Kelvin Liang <kelvin9877@gmail.com>
2019-10-04 02:04:14 +0800

    initial friendship model and add part of the UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.where('id != ?', current_user.id)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
+  get "/friends", to: 'home#friends'
   
   post 'comments/create'
   get 'comments/show'
