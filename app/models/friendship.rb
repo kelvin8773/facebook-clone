27e535a30e3199a7368d110050c09018241c98ed
bright okike <brightokike@gmail.com>
2019-10-10 16:19:55 +0300

    more updates on the friendship milestone6

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,17 +4,7 @@ class Friendship < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
   belongs_to :friend, class_name: 'User', foreign_key: :friend_id
 
-  after_create do |p|
-    Friendship.find_or_create_by!(user_id: p.friend_id, friend_id: p.user_id) 
-  end
-
-
-  after_destroy do |p|
-    reciprocal = Friendship.find_by(user_id: p.friend_id, friend_id: p.user_id)
-    reciprocal.destroy unless reciprocal.nil?
-  end
-
-  # validates :user, uniqueness: { scope: :friend }
+  validates :user, uniqueness: { scope: :friend }
   validate :user_and_friend_are_different
 
   def user_and_friend_are_different
