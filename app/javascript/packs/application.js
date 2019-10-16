a3735ac9f0df11f057b0ed66a07d915426eb29ba
Kelvin Liang <kelvin9877@gmail.com>
2019-10-16 16:00:46 +0800

    update with fontawosome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,16 +9,9 @@ require("@rails/activestorage").start();
 require("channels");
 require("packs/script.js");
 
-import "../stylesheets/application";
 import "bootstrap";
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-
-// Add all icons to the library so you can use it in your page
-library.add(fas, far, fab);
+import "@fortawesome/fontawesome-free/js/all";
+import "../stylesheets/application";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
