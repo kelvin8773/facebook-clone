66cb545f79510cb1bf2d41e63843d7a210105cc0
Kelvin Liang <kelvin9877@gmail.com>
2019-10-16 18:19:31 +0800

    add fontawesome and fix minor changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("packs/script.js");
 
 import "../stylesheets/application";
 import "bootstrap";
+import "@fortawesome/fontawesome-free/js/all";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
